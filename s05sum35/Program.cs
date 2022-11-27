fdcdaab2ea85d240a61521024bef9837c57776b8
Denis Myagkov <tomich-84@mail.ru>
2022-11-27 21:24:06 +0300

    add s05sum35

---
+++ s05sum35/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Console.Clear();
+int[] array = new int[8];
+FillArray(array);
+
+int countEven = CountEvenNumbers(array);
+
+Console.WriteLine($"{String.Join(" ", array)} -> {countEven}");
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+}
+
+int CountEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count += 1;
+    }
+    return count;
+}
